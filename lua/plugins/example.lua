719014324db6608abc5284781cbf79a7ae575447
Ilya Siluyanov <isiluyanov8@gmail.com>
2024-09-06 21:19:45 +0300

    [wip]

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -62,10 +62,8 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
       {
-        "<leader>fp",
+        "<leader>ff",
         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
         desc = "Find Plugin File",
       },
